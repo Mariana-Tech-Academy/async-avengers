17df5cb511ba2651a8650bd32165e7c8f66307b5
samirali17044-pixel <samirali17044@gmail.com>
2026-03-13 21:26:33 +0000

    added invoice and product logic to repo, service and handlers

--- main.go
+++ main.go
@@ -16,16 +16,27 @@ func main() {
 
 	// initialize repositories
 	userRepo := &repository.UserRepo{}
-	
+	businessRepo := &repository.BusinessRepo{}
+	clientRepo := &repository.ClientRepo{}
+	invoiceRepo := &repository.InvoiceRepo{} // added invoice repo
+	productRepo := &repository.ProductRepo{} // added product repo
 
 	// initialize service
 	userService := &services.UserService{Repo: userRepo}
+	businessService := &services.BusinessService{Repo: businessRepo}
+	clientService := &services.ClientService{Repo: clientRepo}
+	invoiceService := &services.InvoiceService{Repo: invoiceRepo} // added invoice service
+	productService := &services.ProductService{Repo: productRepo} // added product service
 
 	// initialize handlers
 	userHandler := &handlers.UserHandler{userService}
+	businessHandler := &handlers.BusinessHandler{businessService}
+	clientHandler := &handlers.ClientHandler{clientService}
+	invoiceHandler := &handlers.InvoiceHandler{invoiceService} // added invoice handler
+	productHandler := &handlers.ProductHandler{productService} // added product handler
 
 	//routes
-	r := routes.SetupRouter(userHandler)
+	r := routes.SetupRouter(userHandler, businessHandler, clientHandler, invoiceHandler, productHandler)  // added invoice and product handlers to routes
 
 	//start server
 	err := http.ListenAndServe(":8080", r)
