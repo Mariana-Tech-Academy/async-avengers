94a094378606fc204400dadb338b777b62f7ec1c
Fatima <faatimaashaarif@gmail.com>
2026-03-23 18:01:47 +0000

    same

--- models/business.go
+++ models/business.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-
 	"gorm.io/gorm"
 )
 
@@ -9,17 +8,16 @@ import (
 type Business struct {
 	gorm.Model
 
-	UserID  uint     `json:"user_id"`
-	Name    string   `json:"name"`
-	Address string   `json:"address"`
-	Phone   string   `json:"phone"`
-	Email   string   `json:"email"`
+	UserID  uint   `json:"user_id"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	Phone   string `json:"phone"`
+	Email   string `json:"email"`
 
-// Business logo stored as a file path or URL
-Logo string `json:"Logo"`
+	// Business logo stored as a file path or URL
+	Logo string `json:"Logo"`
 
-// Adding Tax information
-VATNumber string  `json:"vat_number"`
-TaxRate   float64 `json:"tax_rate"`  
-
+	// Adding Tax information
+	VATNumber string  `json:"vat_number"`
+	TaxRate   float64 `json:"tax_rate"`
 }
