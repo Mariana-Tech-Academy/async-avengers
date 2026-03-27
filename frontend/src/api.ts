92684021143beccb1c38ca82fa436bf3f902fec7
Fatima <faatimaashaarif@gmail.com>
2026-03-27 12:36:45 +0000

    feat: actually saving and adding login logic

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.MODE === 'development' 
-    ? 'http://localhost:8080' 
-    : 'https://async-avengers.onrender.com', 
+    baseURL: import.meta.env.MODE === 'development'
+        ? 'http://localhost:8080'
+        : 'https://async-avengers.onrender.com',
 });
 
 // Add this "interceptor" to attach the token if it exists
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default api;
