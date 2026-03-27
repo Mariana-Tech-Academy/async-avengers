664992a31f5ef12e303f691706f0bc5de101f7bd
Fatima <faatimaashaarif@gmail.com>
2026-03-27 12:05:07 +0000

    Update frontend with API connection

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 // This is the "bridge" to your Go server
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Replace 8080 with the port from your main.go
+    // This checks if you're working on your computer or if the app is live
+    baseURL: import.meta.env.MODE === 'development'
+        ? 'http://localhost:8080'
+        : 'https://async-avengers.onrender.com',
 });
 
 export default api;
